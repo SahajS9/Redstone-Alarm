7a2dca35bd05453333311132015fc5d6b714539c
Sahaj <37724510+SahajS9@users.noreply.github.com>
2019-12-29 12:01:19 -0800

    Update Alarm.lua

--- Alarm.lua
+++ Alarm.lua
@@ -1,13 +1,9 @@
 alarm = require("component").os_alarm
 
 alarm.setAlarm("klaxon2") -- Allows you to change the alarm played, 
---the default config is klaxon1 and klaxon2 and the default sound is klaxon1
+
 
 alarm.setRange(15) 
--- Sets the range in blocks from the alarm block, allowed range is 0-15
 
-alarm.activate() -- It.. activates the alarm.
 
--- alarm.listSounds() -- returns a table of configured alarms.
-
---  alarm.deactivate() -- It.. deactivates the alarm.
+alarm.activate()
