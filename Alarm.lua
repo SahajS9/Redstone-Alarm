c6fabab2b78605e6e72d2fa491254b08714f0925
Sahaj <37724510+SahajS9@users.noreply.github.com>
2019-12-29 12:21:15 -0800

    Update Alarm.lua

--- Alarm.lua
+++ Alarm.lua
@@ -1,9 +1,12 @@
 alarm = require("component").os_alarm
 
 alarm.setAlarm("klaxon1") 
-alarm.setRange(15) 
-alarm.activate()
+alarm.setRange(15)
 
-os.sleep(10)
+getInput():
+--if
+--alarm.activate()
 
-alarm.deactivate()
+--os.sleep(10)
+
+--alarm.deactivate()
