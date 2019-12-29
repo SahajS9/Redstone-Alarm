8f96b8e21a68b146344e52594cc02f0c356e620a
Sahaj <37724510+SahajS9@users.noreply.github.com>
2019-12-29 14:23:18 -0800

    Update Alarm.lua

--- Alarm.lua
+++ Alarm.lua
@@ -1,6 +1,6 @@
 alarm = require("component").os_alarm
 redstone = require("component").redstone
-require(sides)
+sides =require("component").redstone
 local signal
 alarm.setAlarm("klaxon1") 
 alarm.setRange(15)
