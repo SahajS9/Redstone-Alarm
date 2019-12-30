a5bd4ce122eb6b15659bd78b53ac4d2d400a79ac
Sahaj <37724510+SahajS9@users.noreply.github.com>
2019-12-30 01:05:42 -0800

    Update Alarm.lua

--- Alarm.lua
+++ Alarm.lua
@@ -1,16 +1,18 @@
-component = require("component")
-alarm = component.os_alarm
-redstone = component.redstone
-sides = require("sides")
+local component = require("component")
+local alarm = component.os_alarm
+local redstone = component.redstone
+local sides = require("sides")
 local signal
+
 alarm.setAlarm("klaxon1") 
 alarm.setRange(0)
 
 while true do
-  signal = redstone.getInput(sides.back)
+  signal = redstone.getInput(sides.-side-)
   if signal > 0 
   then
     alarm.activate()
+    os.sleep(
   --print("Alarm active.")
   else
     alarm.deactivate()
